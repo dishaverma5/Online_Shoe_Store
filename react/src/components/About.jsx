84be53c5221c07de491e9ffdd3e8d8af9bcb2e65
Paris Machado <parismachado101@gmail.com>
2024-07-11 11:19:54 -0400

    shoesss

---
+++ react/src/components/About.jsx
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Welcome to <strong>The Step Up Shoe Store</strong> - the best place to sooth your sole!
+        
+      </p>
+      <p>
+        Will add description later
+      </p>
+    </div>
+  );
+};
+
+export default About;
