3b1615129d266e77937349bef812f0138465c07a
Paris Machado <parismachado101@gmail.com>
2024-07-12 13:52:42 -0400

    stuyffs

--- react/src/components/Product.jsx
+++ react/src/components/Product.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-const Product = ({ product }) => {
+const Product = ({ product, addToCart }) => {
+  const handleAddToCart = () => {
+    addToCart(product);
+  };
+
   return (
     <div className="card">
       <img src={product.image} className="card-img-top" alt={product.name} />
@@ -8,7 +12,9 @@ const Product = ({ product }) => {
         <h5 className="card-title">{product.name}</h5>
         <p className="card-text">Price: $ {product.price}</p>
         <p className="card-text">Category: {product.category}</p>
-        <button className="btn btn-primary">Add to Cart</button>
+        <button className="btn btn-primary" onClick={handleAddToCart}>
+          Add to Cart
+        </button>
       </div>
     </div>
   );
