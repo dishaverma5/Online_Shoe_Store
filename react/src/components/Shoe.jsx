82221c4e93218ebb85aef5212984878cfc13e570
Paris Machado <parismachado101@gmail.com>
2024-07-12 11:27:59 -0400

    shoe changes AGAIN

--- react/src/components/Shoe.jsx
+++ react/src/components/Shoe.jsx
@@ -1,42 +1,38 @@
 import React from "react";
 
 const Shoe = (props) => {
+  const { shoeDetails, additionalFeatures, addedTimestamp, _id } = props.data;
+
   return (
     <div
       className="card card-background"
       style={{ flex: "1", minWidth: "300px", maxWidth: "45%" }}
     >
       <div className="card-body">
-        <h5 className="card-title">Sock Details</h5>
-        <div className="card-text">Shoe Size: {props.data.shoeDetails.size}</div>
-        <div className="card-text">Shoe Color: {props.data.shoeDetails.color}</div>
-        <div className="card-text">
-          Shoe Brand: {props.data.shoeDetails.pattern}
-        </div>
-        <div className="card-text">
-          Shoe Material: {props.data.shoeDetails.material}
-        </div>
-        <div className="card-text">
-          Shoe Condition: {props.data.shoeDetails.condition}
-        </div>
+        <h5 className="card-title">Shoe Details</h5>
+        <div className="card-text">Shoe Size: {shoeDetails?.size}</div>
+        <div className="card-text">Shoe Color: {shoeDetails?.color}</div>
+        <div className="card-text">Shoe Brand: {shoeDetails?.brand}</div>
+        <div className="card-text">Shoe Material: {shoeDetails?.material}</div>
+        <div className="card-text">Shoe Condition: {shoeDetails?.condition}</div>
       </div>
       <div className="card-body">
-        <h5 className="card-title">ADDITIONAL FEATURES</h5>
+        <h5 className="card-title">Additional Features</h5>
         <div className="card-text">
-          Has Laces: {props.data.additionalFeatures.hasLaces ? "Yes" : "No"}
+          Has Laces: {additionalFeatures?.hasLaces ? "Yes" : "No"}
         </div>
         <div className="card-text">
-          Is Athletic: {props.data.additionalFeatures.isAthletic ? "Yes" : "No"}
+          Is Athletic: {additionalFeatures?.isAthletic ? "Yes" : "No"}
         </div>
       </div>
       <div
         className="card-footer"
         style={{ display: "flex", justifyContent: "space-between" }}
       >
-        <small className="text-muted">Added: {props.data.addedTimestamp}</small>
+        <small className="text-muted">Added: {new Date(addedTimestamp).toLocaleString()}</small>
         <button
           className="btn btn-sm btn-danger"
-          onClick={() => props.handleDelete(props.data._id)}
+          onClick={() => props.handleDelete(_id)}
         >
           Delete
         </button>
