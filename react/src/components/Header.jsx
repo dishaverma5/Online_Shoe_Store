be24d0c40e857fdb33a12d7955c6ff79cc5db64e
Paris Machado <parismachado101@gmail.com>
2024-07-15 11:51:46 -0400

    why is app broken???

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -1,55 +1,56 @@
+ 
+
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import Search from "./components/Search";
-import { AuthProvider } from "./hooks/AuthContext";
+import { Link } from "react-router-dom"; // If using React Router for navigation
 
 const Header = () => {
-  const location = useLocation();
-
-  // Determine if we are on the Home page
-  const isHomePage = location.pathname === "/";
-
   return (
-    <nav className={`navbar navbar-expand-lg bg-body-tertiary ${isHomePage ? "full-width" : ""}`}>
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          <h2 style={{ fontFamily: "Didot", color: "#003f69" }}>
-            <b>S T E P - U P</b>
-          </h2>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                <b>HOME</b>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                <b>ABOUT</b>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/cart">
-                <b>MY CART</b>
-              </Link>
-            </li>
-          </ul>
-          <Search />
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <span >
+              <b>STEP UP SHOES </b>
+            </span>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  HOME
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/products">
+                  PRODUCTS
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">
+                  CART
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/checkout">
+                  CHECKOUT
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 };
 
