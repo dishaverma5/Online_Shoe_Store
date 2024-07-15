0da6ec600d0482fde14a30257b4363dd750be276
Paris Machado <parismachado101@gmail.com>
2024-07-15 13:42:17 -0400

    Header changes

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -15,8 +15,8 @@ const Header = () => {
           <button
             className="navbar-toggler"
             type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
             aria-controls="navbarNav"
             aria-expanded="false"
             aria-label="Toggle navigation"
@@ -24,7 +24,7 @@ const Header = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto w-100 justify-content-end">
+            <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 <Link className="nav-link" to="/">
                   HOME
