03d0f394ef58a7f909dd05d03337df090c38bf1d
Paris Machado <parismachado101@gmail.com>
2024-07-15 20:43:07 -0400

    test

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/shoes`);
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/shoe`);
         if (!response.ok) {
           throw new Error('Data could not be fetched!');
         }
