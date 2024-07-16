a73257367abee0f9e28cb0923f74460ebce731bf
Paris Machado <parismachado101@gmail.com>
2024-07-15 21:35:45 -0400

    home new

--- react/src/components/Home.jsx
+++ react/src/components/Home.jsx
@@ -3,7 +3,21 @@ import Featured from "./Featured";
 import Categories from "./Categories";
 import Shoe from "./Shoe";
 
-const Home = ({ data, categories, page, setPage, addToCart }) => {
+const Home = ({ data, categories, addToCart }) => {
+  const groupBy = (array, key) => {
+    return array.reduce((result, currentValue) => {
+      (result[currentValue[key]] = result[currentValue[key]] || []).push(
+        currentValue
+      );
+      return result;
+    }, {});
+  };
+
+  const groupedByBrand = groupBy(data, "shoeDetails.brand");
+  const groupedByColor = groupBy(data, "shoeDetails.color");
+  const groupedBySize = groupBy(data, "shoeDetails.size");
+  const groupedByType = groupBy(data, "shoeDetails.shoe_type");
+
   return (
     <>
       <div className="row">
@@ -20,10 +34,69 @@ const Home = ({ data, categories, page, setPage, addToCart }) => {
           />
         </div>
       </div>
+      <hr />
       <div className="row">
-        {data.map((product) => (
-          <Shoe key={product.id} product={product} addToCart={addToCart} />
-        ))}
+        <div className="col">
+          <h3>Grouped by Brand</h3>
+          {Object.keys(groupedByBrand).map((brand) => (
+            <div key={brand}>
+              <h4>{brand}</h4>
+              <div className="row">
+                {groupedByBrand[brand].map((product) => (
+                  <Shoe key={product.shoe_id} product={product} addToCart={addToCart} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col">
+          <h3>Grouped by Color</h3>
+          {Object.keys(groupedByColor).map((color) => (
+            <div key={color}>
+              <h4>{color}</h4>
+              <div className="row">
+                {groupedByColor[color].map((product) => (
+                  <Shoe key={product.shoe_id} product={product} addToCart={addToCart} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col">
+          <h3>Grouped by Size</h3>
+          {Object.keys(groupedBySize).map((size) => (
+            <div key={size}>
+              <h4>{size}</h4>
+              <div className="row">
+                {groupedBySize[size].map((product) => (
+                  <Shoe key={product.shoe_id} product={product} addToCart={addToCart} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col">
+          <h3>Grouped by Type</h3>
+          {Object.keys(groupedByType).map((type) => (
+            <div key={type}>
+              <h4>{type}</h4>
+              <div className="row">
+                {groupedByType[type].map((product) => (
+                  <Shoe key={product.shoe_id} product={product} addToCart={addToCart} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
